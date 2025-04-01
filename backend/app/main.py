1743402e64e00186f877b5fca386211e15943768
hecdaza <144064932+hecdaza@users.noreply.github.com>
2025-04-01 11:17:50 -0300

    Add files via upload

---
+++ backend/app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import clima, farmacias, bnc, indicadores, auth
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router)
+app.include_router(clima.router)
+app.include_router(farmacias.router)
+app.include_router(bnc.router)
+app.include_router(indicadores.router)
+
+@app.get("/")
+def root():
+    return {"message": "API backend operativo"}
