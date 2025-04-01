49b5885a79c06ae572c41c5832f43650540fc1c4
hecdaza <144064932+hecdaza@users.noreply.github.com>
2025-04-01 13:40:32 -0300

    Update Farmacias.jsx

--- frontend/src/pages/Farmacias.jsx
+++ frontend/src/pages/Farmacias.jsx
@@ -5,7 +5,7 @@ function Farmacias() {
   const [datos, setDatos] = useState(null)
 
   useEffect(() => {
-    axios.get('http://localhost:8000/farmacias/')
+    axios.get('https://mi-web-fastapi.onrender.com/farmacias')
       .then(res => setDatos(res.data))
       .catch(() => setDatos({ error: 'No se pudieron cargar las farmacias.' }))
   }, [])
